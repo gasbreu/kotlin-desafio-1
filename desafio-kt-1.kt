d95d9731d15d4be416ab70681242d8b5b80794b2
Gabriel Fernandes <96575258+gasbreu@users.noreply.github.com>
2023-10-06 23:13:22 -0300

    Update desafio-kt-1.kt

--- desafio-kt-1.kt
+++ desafio-kt-1.kt
@@ -1,26 +1,36 @@
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Level { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuários(val nome: String, val id: Int) {
-    fun matricular(usuario: Usuários) {
-        val usuarios = mutableListOf<Usuários>()
-            usuarios.add(usuario)
-    }
-}
+class Usuario(val nome: String) 
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+
+    val inscritos = mutableListOf<Usuario>()
+    
+    fun matricular(usuario: Usuario) {
+            inscritos.add(usuario)
+            println("$usuario foi matriculado da formação: $nome")
+    }
+
+    fun desmatricular(usuario: Usuario) {
+        inscritos.remove(usuario)
+        println("$usuario foi desmatriculado da formação: $nome")
+    }
+}
+
+fun main() {
     val contEdu1 = ConteudoEducacional("Conhecendo o Kotlin e Sua Documentação Oficial", 60)
     val contEdu2 = ConteudoEducacional("Princípios de Interface de Usuário e Layouts Android", 120)
     val contEdu3 = ConteudoEducacional("Visão Geral Sobre o Android Jetpack", 120)
 
-data class Formação(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val formacao = Formação("Desenvolvimento Android", listOf(contEdu1, contEdu2, contEdu3))
-}
-
-fun main() {
-    val usuarios = Usuários("mopa,", 1)
+    val formacao = Formacao("Desenvolvimento Android", listOf(contEdu1, contEdu2, contEdu3))
     
-    usuarios.matricular(usuarios)
-    println(usuarios)
+    val usuario1 = Usuario("Gabriel")
+    val usuario2 = Usuario("Vanessa")
+    
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    
+    formacao.inscritos.forEach { println(it.nome) }
 }
